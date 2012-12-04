ce8c437135d7ad5792dbd18906bd43ece86aeedb
Richard Hull <rm_hull@yahoo.co.uk>
2012-12-04 22:11:12 +0000

    Use a canvas path rather than disjoint rectangles

--- src/maze/client/core.cljs
+++ src/maze/client/core.cljs
@@ -1,7 +1,7 @@
 (ns maze.client.core
   (:require [fetch.remotes :as remotes])
   (:require-macros [fetch.macros :as fm])
-  (:use [monet.canvas :only [get-context fill-style circle rect alpha begin-path close-path fill]]
+  (:use [monet.canvas :only [get-context stroke stroke-style stroke-cap begin-path close-path line-to move-to stroke-width]]
         [jayq.core :only [$ document-ready data attr]]))
 
 (defn coord->pos [[^long x ^long y] [^long w ^long h]]
@@ -9,25 +9,35 @@
     (* (rem y h) w)
     (rem x w)))
 
-(defn north [ctx x y len]
-  (rect ctx {:x (* x len) :y (* y len) :w (+ len 2) :h 2}))
+(defn draw-cell [ctx x y len walls]
+  (let [x (inc (* x len))
+        y (inc (* y len))]
+    (when (:north walls) (-> ctx (move-to x y) (line-to (+ x len) y)))
+    (when (:west walls)  (-> ctx (move-to x y) (line-to x (+ y len))))
+    ctx))
 
-(defn west [ctx x y len]
-  (rect ctx {:x (* x len) :y (* y len) :w 2 :h (+ len 2)}))
-
-(defn draw [ctx maze len]
+(defn draw-cells [ctx maze len]
   (let [[w h] (:size maze)]
-    (.log js/console maze) 
-    (-> ctx
-        (fill-style "#606060")
-        (rect {:x 0 :y (* h len) :w (+ (* w len) 2) :h 2})
-        (rect {:x (* w len) :y 0 :w 2 :h (* h len)})
-      )
     (doseq [[idx walls] (map vector (iterate inc 0) (:data maze))
             :let [x (rem idx w)
                   y (rem (quot idx w) h)]]
-      (if (:north walls) (north ctx x y len))
-      (if (:west  walls) (west  ctx x y len)))))
+      (draw-cell ctx x y len walls)) 
+    ctx))
+
+(defn draw-maze [ctx maze len]
+  (let [[w h] (:size maze)]
+    (-> ctx
+        (stroke-width 2)
+        (stroke-cap "square")
+        (stroke-style "#606060")
+        (begin-path)
+        (move-to 0 (inc (* h len)))
+        (line-to (inc (* w len)) (inc  (* h len)))
+        (line-to (inc  (* w len)) 0)
+        (draw-cells maze len)   
+        (stroke)
+        (close-path))
+    ctx))
 
 (document-ready
   (fn []
@@ -41,4 +51,4 @@
           (attr :width (+ 2 (* cell-size width)))
           (attr :height (+ 2 (* cell-size height))))
       (fm/remote (generate-maze width height) [maze] 
-        (draw ctx maze cell-size)))))
+        (draw-maze ctx maze cell-size)))))
