2f10cf73867e366d17d9ee7b2b781728e12f81a5
Richard Hull <rm_hull@yahoo.co.uk>
2012-12-11 22:24:12 +0000

    Use latest snapshot versions of monet & ring-gzip-middleware

--- project.clj
+++ project.clj
@@ -8,8 +8,8 @@
                  [jayq "0.3.2"]
                  [crate "0.2.1"]
                  [fetch "0.1.0-alpha2"]
-                 [ibdknox/ring-gzip-middleware "0.1.1"]
-                 [org.clojars.rm-hull/monet "0.1.2-SNAPSHOT"]]
+                 [rm-hull/ring-gzip-middleware "0.1.4-SNAPSHOT"]
+                 [rm-hull/monet "0.1.2-SNAPSHOT"]]
   :cljsbuild {
     :builds [
       {:source-path "src/maze/client"
