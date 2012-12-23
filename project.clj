729e9ea1c122b964204f0370488a0a462422f1dc
Richard Hull <rm_hull@yahoo.co.uk>
2012-12-23 21:06:35 +0000

    Latest jars

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
   :url "http://maze.destructuring-bind.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.5.0-RC1"]
                  [org.clojure/data.priority-map "0.0.2"]
                  [org.clojure/core.cache "0.6.2"]
                  [noir "1.3.0-beta10"]
-                 [jayq "0.3.2"]
-                 [crate "0.2.1"]
+                 [jayq "1.0.0"]
+                 [crate "0.2.3"]
                  [fetch "0.1.0-alpha2"]
                  [rm-hull/ring-gzip-middleware "0.1.4-SNAPSHOT"]
-                 [rm-hull/monet "0.1.2-SNAPSHOT"]]
+                 [rm-hull/monet "0.1.3-SNAPSHOT"]]
   :cljsbuild {
     :builds [
       {:source-path "src/maze/client"
@@ -20,7 +20,7 @@
                   :print-input-delimiter true
                   :pretty-print true}}]}
   :hooks [leiningen.cljsbuild]
-  :plugins [[lein-cljsbuild "0.2.9"]]
+  :plugins [[lein-cljsbuild "0.2.10"]]
   :profiles {:dev {:dependencies [[vimclojure/server "2.3.6"]]}}
   :main maze.server
   :min-lein-version "2.0.0"
