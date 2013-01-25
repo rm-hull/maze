c1c517bf896ca998aa4d110126ab1f96b60f3863
Richard Hull <rm_hull@yahoo.co.uk>
2013-01-25 23:56:56 +0000

    Stop using trampoline

--- project.clj
+++ project.clj
@@ -9,18 +9,18 @@
                  [noir "1.3.0"]
                  [jayq "2.0.0"]
                  [crate "0.2.3"]
-                 [rm-hull/fetch "0.1.1-SNAPSHOT"]
                  [rm-hull/ring-gzip-middleware "0.1.4-SNAPSHOT"]
+                 [rm-hull/fetch "0.1.1-SNAPSHOT"]
                  [rm-hull/monet "0.1.3-SNAPSHOT"]]
   :cljsbuild
     {:builds
      [{:source-paths ["src/maze/client"],
        :compiler
-       {:pretty-print true,
+       {:pretty-print false,
         :output-to "resources/public/cljs/maze.js",
         :externs ["externs/jquery.js"],
         :optimizations :advanced,
-        :print-input-delimiter true}}]}
+        :print-input-delimiter false}}]}
   :hooks [leiningen.cljsbuild]
   :plugins [[lein-cljsbuild "0.3.0"]]
   :profiles {:dev {:dependencies [[vimclojure/server "2.3.6"]]}}
