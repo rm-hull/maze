e0b12e41595eb9f633bcd8b7aa0f14d0edeabbae
Richard Hull <rm_hull@yahoo.co.uk>
2013-01-26 00:20:31 +0000

    clojure RC3

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :url "http://maze.destructuring-bind.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.5.0-RC3"]
                  [org.clojure/data.priority-map "0.0.2"]
                  [org.clojure/core.cache "0.6.2"]
                  [ns-tracker "0.2.1"]
